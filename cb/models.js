e73c20ca8e7c59b4e584b529e395687fb1afd884
vertical73 <40932391+vertical73@users.noreply.github.com>
2021-02-01 05:47:11 -0500

    kinkyali

--- cb/models.js
+++ cb/models.js
@@ -1,10 +1,10 @@
 var models = [
     "evasasha",
     "_virtual_lady_",
+    "kinkyali",
     "wild_schoolgirl",
     "_milkyway",
     "agatha137",
-    "alice_kosmos",
     "lilly_vanilla",
     "lola_bunny94"
 ];
