261711311bc599f8eb41b7ce1509a10332c869cd
Jonathan Strange <parallel42@gmail.com>
2021-08-07 18:21:27 -0400

    inits cb0

---
+++ cb0/functions.js
@@ -0,0 +1,18 @@
+
+
+function refreshChannel(model) {
+	// TODO
+}
+
+function initChannel(model, priority) {
+	// TODO
+}
+
+function updateChannel(model, priority) {
+	// TODO
+}
+
+function isOfflineImage(imgSize) {
+	return (imgSize == 0 || imgSize == 7442 || imgSize == 21971 || imgSize == 4824 || imgSize == 6778 || imgSize == 6734);
+}
+
