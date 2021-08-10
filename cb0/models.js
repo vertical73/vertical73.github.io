acb1bfede2908e835c9dd764c299832dd93cdc4a
Jonathan Strange <parallel42@gmail.com>
2021-08-10 13:58:23 -0400

    heyhorny_cb

--- cb0/models.js
+++ cb0/models.js
@@ -59,7 +59,7 @@ var $tiffanyhouston_ = new Model("tiffanyhouston_", 22); $$__Models.set($tiffany
 let tierA = ["evasasha","july_koss","_milkyway","mango_shake","lilly_vanilla","candysfox","yournaughtymiss","annie_sweetyxx","cogitademe","lovely_aria","alice_kosmos","lola_bunny94","juicekatee","anna_sabotage","thepleasureofsex","sweet__sugar","wild_schoolgirl","lolla_molla","hell_lo","hayle_green","_woweva__","catanddickxxx"];
 tierA.forEach(m => addModel(m, 23));
 
-let tierB = ["jessica_jonses","vivian_clark","kinkyali","feelmemore","_virtual_lady_","little_vee","xllodaz","blackmango_mary","tiffanyhouston_","marylanex","liisppb","sweet_tinker_bell","agatha137","evelissa","kleasure","bettybonhill","alicewood_","amazon_girl","melisasweety","funnysimka","qeensgambit","angelinamoore","rennatha21","tonibrock","selfish_ashley","_love_daddys","amazing_roxana","ms_seductive","miley_k18"];
+let tierB = ["jessica_jonses","vivian_clark","kinkyali","feelmemore","_virtual_lady_","little_vee","xllodaz","blackmango_mary","tiffanyhouston_","marylanex","liisppb","sweet_tinker_bell","agatha137","evelissa","kleasure","bettybonhill","alicewood_","amazon_girl","melisasweety","funnysimka","qeensgambit","angelinamoore","rennatha21","tonibrock","selfish_ashley","_love_daddys","amazing_roxana","heyhorny_cb","ms_seductive","miley_k18"];
 tierB.forEach(m => addModel(m, 30));
 
 const $$__Params = new URLSearchParams(window.location.search);
