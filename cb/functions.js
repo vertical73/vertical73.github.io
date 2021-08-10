5c038b69abdf32e8000b3a432793d1f8fd19a720
Jonathan Strange <parallel42@gmail.com>
2021-08-10 19:19:05 -0400

    cb0 -> cb

---
+++ cb/functions.js
@@ -0,0 +1,68 @@
+
+function channelId(modelName) {
+	return "channel$" + modelName;
+}
+
+function channelImgId(modelName) {
+	return "img$" + modelName;
+}
+
+function channelImgUrl(modelName) {
+	return "https://roomimg.stream.highwebmedia.com/ri/" + modelName + ".jpg";
+}
+
+function channelActiveUrl(modelName) {
+	return "https://cbjpeg.stream.highwebmedia.com/minifwap/" + modelName + ".jpg?f=" + Math.random();
+}
+
+function chaturbateUrl(modelName) {
+	return "https://m.chaturbate.com/" + modelName + "/";
+}
+
+function createChannelBox(model, priorityOverride) {
+	const channelBox = document.createElement('div');
+	channelBox.id = channelId(model.name);
+	channelBox.className = "model";
+	
+	let priority;
+	if (priorityOverride === undefined) {
+		priority = model.priority;
+	} else {
+		console.log(model.name + " -> " + priorityOverride);
+		priority = priorityOverride;
+	}
+	channelBox.style.order = priority;
+
+	channelBox.style.display = 'none';
+	
+	return channelBox;
+}
+
+function createAnchor(modelName) {
+	const anchor = document.createElement('a');
+	anchor.href = chaturbateUrl(modelName);
+	anchor.target = "_blank";
+	
+	return anchor;
+}
+
+function createImg(modelName) {
+	const img = new Image();
+	img.id = channelImgId(modelName);
+	img.src = channelActiveUrl(modelName);
+	
+	return img;
+}
+
+function turnOn(modelName, channelBox) {
+	channelBox.style.display = 'inline';
+}
+
+function turnOff(channelBox) {
+	channelBox.style.display = 'none';
+}
+
+function isOfflineImage(imgSize) {
+	return (imgSize == 0 || imgSize == 4456 || imgSize == 7442 || imgSize == 21971 || imgSize == 4824 || imgSize == 6778 || imgSize == 6734);
+}
+
