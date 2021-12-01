82887737532b1f45270d237099a3c00fa117cfc6
Jonathan Strange <parallel42@gmail.com>
2021-12-01 02:17:02 -0500

    (spaces)

--- cb0/functions.js
+++ cb0/functions.js
@@ -1,129 +1,129 @@
 class Model {
-	constructor(name, priority, scoring, pvt) {
-		this.name = name;
-		this.priority = priority; // number, sort ascending (lower is better)
-		this.scoring = scoring; // optional
-		this.pvt = pvt; // optional
-	}
+    constructor(name, priority, scoring, pvt) {
+        this.name = name;
+        this.priority = priority; // number, sort ascending (lower is better)
+        this.scoring = scoring; // optional
+        this.pvt = pvt; // optional
+    }
 }
 
 class Scoring {
-	constructor(beauty, dtf, levels, sensitivity, authenticity, exhibition, shape, mangos, peach, bird, greek, broadcast, availability) {
-		// number, sort descending (higher is better)
-		this.beauty = beauty; // {10..1}
-		this.levels = levels; // {5..0}
-		this.sensitivity = sensitivity; // {5..0}
-		this.authenticity = authenticity; // {5..0}
-		this.dtf = dtf; // {5..0}
-		this.exhibition = exhibition; // {5..0}
-		this.shape = shape; // {5..0}
-		this.mangos = mangos; // {5..0}
-		this.peach = peach; // {5..0}
-		this.bird = bird; // {5..0}
-		this.greek = greek; // {5..0}
-		this.broadcast = broadcast; // {5..0}
-		this.availability = availability; // {5..0}
-	}
+    constructor(beauty, dtf, levels, sensitivity, authenticity, exhibition, shape, mangos, peach, bird, greek, broadcast, availability) {
+        // number, sort descending (higher is better)
+        this.beauty = beauty; // {10..1}
+        this.levels = levels; // {5..0}
+        this.sensitivity = sensitivity; // {5..0}
+        this.authenticity = authenticity; // {5..0}
+        this.dtf = dtf; // {5..0}
+        this.exhibition = exhibition; // {5..0}
+        this.shape = shape; // {5..0}
+        this.mangos = mangos; // {5..0}
+        this.peach = peach; // {5..0}
+        this.bird = bird; // {5..0}
+        this.greek = greek; // {5..0}
+        this.broadcast = broadcast; // {5..0}
+        this.availability = availability; // {5..0}
+    }
 }
 
 class Pvt { 
-	constructor(allows, rate, recording) {
-		this.allows = allows; // boolean
-		this.rate = rate; // number
-		this.recording = recording; // "yes", "no", "sometimes"
-	}
+    constructor(allows, rate, recording) {
+        this.allows = allows; // boolean
+        this.rate = rate; // number
+        this.recording = recording; // "yes", "no", "sometimes"
+    }
 }
 
 const $$__Models = new Map();
 const $$__Params = new URLSearchParams(window.location.search);
 
 function channelId(modelName) {
-	return "channel$" + modelName;
+    return "channel$" + modelName;
 }
 
 function channelImgId(modelName) {
-	return "img$" + modelName;
+    return "img$" + modelName;
 }
 
 function channelImgUrl(modelName) {
-	return "https://roomimg.stream.highwebmedia.com/ri/" + modelName + ".jpg";
+    return "https://roomimg.stream.highwebmedia.com/ri/" + modelName + ".jpg";
 }
 
 function channelActiveUrl(modelName) {
-	return "https://cbjpeg.stream.highwebmedia.com/minifwap/" + modelName + ".jpg?f=" + Math.random();
+    return "https://cbjpeg.stream.highwebmedia.com/minifwap/" + modelName + ".jpg?f=" + Math.random();
 }
 
 function chaturbateUrl(modelName) {
-	return "https://m.chaturbate.com/" + modelName + "/";
+    return "https://m.chaturbate.com/" + modelName + "/";
 }
 
 function createChannelBox(model, priorityOverride) {
-	const channelBox = document.createElement('div');
-	channelBox.id = channelId(model.name);
-	channelBox.className = "model";
-	
-	let priority;
-	if (priorityOverride === undefined) {
-		priority = model.priority;
-	} else {
-		console.log(model.name + " -> " + priorityOverride);
-		priority = priorityOverride;
-	}
-	channelBox.style.order = priority;
+    const channelBox = document.createElement('div');
+    channelBox.id = channelId(model.name);
+    channelBox.className = "model";
+    
+    let priority;
+    if (priorityOverride === undefined) {
+        priority = model.priority;
+    } else {
+        console.log(model.name + " -> " + priorityOverride);
+        priority = priorityOverride;
+    }
+    channelBox.style.order = priority;
 
-	channelBox.style.display = 'none';
-	
-	return channelBox;
+    channelBox.style.display = 'none';
+    
+    return channelBox;
 }
 
 function createAnchor(modelName) {
-	const anchor = document.createElement('a');
-	anchor.href = chaturbateUrl(modelName);
-	anchor.target = "_blank";
-	
-	return anchor;
+    const anchor = document.createElement('a');
+    anchor.href = chaturbateUrl(modelName);
+    anchor.target = "_blank";
+    
+    return anchor;
 }
 
 function createImg(modelName) {
-	const img = new Image();
-	img.id = channelImgId(modelName);
-	img.src = channelActiveUrl(modelName);
-	
-	return img;
+    const img = new Image();
+    img.id = channelImgId(modelName);
+    img.src = channelActiveUrl(modelName);
+    
+    return img;
 }
 
 function turnOn(modelName, channelBox) {
-	channelBox.style.display = 'inline';
+    channelBox.style.display = 'inline';
 }
 
 function turnOff(channelBox) {
-	channelBox.style.display = 'none';
+    channelBox.style.display = 'none';
 }
 
 function isOfflineImage(imgSize) {
-	return (imgSize == 0 || imgSize == 4456 || imgSize == 7442 || imgSize == 21971 || imgSize == 4824 || imgSize == 6778 || imgSize == 6734);
+    return (imgSize == 0 || imgSize == 4456 || imgSize == 7442 || imgSize == 21971 || imgSize == 4824 || imgSize == 6778 || imgSize == 6734);
 }
 
 function addModel(modelName, priority) {
-	if (! $$__Models.has(modelName)) {
-		let newModel = new Model(modelName, priority); 
-		$$__Models.set(modelName, newModel);
-	}
+    if (! $$__Models.has(modelName)) {
+        let newModel = new Model(modelName, priority); 
+        $$__Models.set(modelName, newModel);
+    }
 }
 
 function makeAddendums() {
-	let modelKeys = ['model','m'];
-	for (k of modelKeys) {
-		let moar = $$__Params.getAll(k);
-		for (m of moar) {
-			if (! $$__Models.has(m)) {
-				addModel(m, 110);
-			}
-		}
-	}
-	for (h of $$__Params.getAll('h')) {
-		if ($$__Models.has(h)) {
-			$$__Models.delete(h);
-		}
-	}	
+    let modelKeys = ['model','m'];
+    for (k of modelKeys) {
+        let moar = $$__Params.getAll(k);
+        for (m of moar) {
+            if (! $$__Models.has(m)) {
+                addModel(m, 110);
+            }
+        }
+    }
+    for (h of $$__Params.getAll('h')) {
+        if ($$__Models.has(h)) {
+            $$__Models.delete(h);
+        }
+    }    
 }
--- cb0/render.js
+++ cb0/render.js
@@ -5,129 +5,129 @@ const $$__ImgSizeHistory = new Map(); // model.name -> [imgSize]
 
 const $$__imgHistMax = 4;
 
-function scanChannel(modelName, imgBox) {	
-	let imgScan = fetch(channelActiveUrl(modelName))
-		.then(
-			function(response) {
-				return response.blob();
-			}
-		).then(
-			function(blob) {
-				return blob.size;
-			}
-		);
-	
-	return imgScan.then(
-			function(imgSize) {				
-				let imgSizeArr;
-				if ($$__ImgSizeHistory.has(modelName)) {
-					imgSizeArr = $$__ImgSizeHistory.get(modelName);
-					if (imgSizeArr.length >= $$__imgHistMax) {
-						imgSizeArr.shift();
-					}
-					imgSizeArr.push(imgSize);
-				} else {
-					imgSizeArr = [imgSize];
-				}
-				$$__ImgSizeHistory.set(modelName, imgSizeArr);
-				
-				console.log("[" + Date.now() + "] " + modelName + " -> " + imgSizeArr);
+function scanChannel(modelName, imgBox) {    
+    let imgScan = fetch(channelActiveUrl(modelName))
+        .then(
+            function(response) {
+                return response.blob();
+            }
+        ).then(
+            function(blob) {
+                return blob.size;
+            }
+        );
+    
+    return imgScan.then(
+            function(imgSize) {                
+                let imgSizeArr;
+                if ($$__ImgSizeHistory.has(modelName)) {
+                    imgSizeArr = $$__ImgSizeHistory.get(modelName);
+                    if (imgSizeArr.length >= $$__imgHistMax) {
+                        imgSizeArr.shift();
+                    }
+                    imgSizeArr.push(imgSize);
+                } else {
+                    imgSizeArr = [imgSize];
+                }
+                $$__ImgSizeHistory.set(modelName, imgSizeArr);
+                
+                console.log("[" + Date.now() + "] " + modelName + " -> " + imgSizeArr);
 
-				let isStreaming = (
-					(imgSizeArr.length < $$__imgHistMax)
-					|| (! imgSizeArr.every(s => (s === imgSize)))
-				);
+                let isStreaming = (
+                    (imgSizeArr.length < $$__imgHistMax)
+                    || (! imgSizeArr.every(s => (s === imgSize)))
+                );
 
-				if (isStreaming && (! isOfflineImage(imgSize))) {
-					if (! $$__Active.has(modelName)) {
-						$$__Active.set(modelName, imgBox);
-						turnOn(modelName, $$__Channels.get(modelName));
-						imgBox.src = channelActiveUrl(modelName);
-						pulseThrobber();
-					}
-					if ($$__Offline.has(modelName)) {
-						$$__Offline.delete(modelName);
-					}
-				} else {
-					$$__Offline.set(modelName, imgBox);
-					$$__Active.delete(modelName);
-					turnOff($$__Channels.get(modelName));
-					// pulseThrobber();
-				}
-			}
-		);
+                if (isStreaming && (! isOfflineImage(imgSize))) {
+                    if (! $$__Active.has(modelName)) {
+                        $$__Active.set(modelName, imgBox);
+                        turnOn(modelName, $$__Channels.get(modelName));
+                        imgBox.src = channelActiveUrl(modelName);
+                        pulseThrobber();
+                    }
+                    if ($$__Offline.has(modelName)) {
+                        $$__Offline.delete(modelName);
+                    }
+                } else {
+                    $$__Offline.set(modelName, imgBox);
+                    $$__Active.delete(modelName);
+                    turnOff($$__Channels.get(modelName));
+                    // pulseThrobber();
+                }
+            }
+        );
 }
 
 function refreshChannel(img, modelName, map) {
-	img.src = channelActiveUrl(modelName);
+    img.src = channelActiveUrl(modelName);
 }
 
 function refreshing() {
-	$$__Active.forEach(refreshChannel);
+    $$__Active.forEach(refreshChannel);
 }
 
 function check(img, modelName, imgMap) {
-	scanChannel(modelName, img);
+    scanChannel(modelName, img);
 }
 
 function checking(map) {
-	map.forEach(check);
+    map.forEach(check);
 }
 
 function renderChannel(model, modelName, models) {
-	if (! $$__Channels.has(modelName)) {
-		let tvBox = document.getElementById("main");
-	
-		let channelBox;
-		if ($$__Params.getAll('shuffle').length > 0) {
-			let shuffledPriority = Math.floor(Math.random() * 100) + model.priority;
-			console.log(modelName + " -> " + shuffledPriority);
-			channelBox = createChannelBox(model, shuffledPriority);
-		} else {
-			channelBox = createChannelBox(model);
-		}
-	
-		let anchor = createAnchor(modelName);
-		let img = createImg(modelName);
-	
-		anchor.appendChild(img);
-		channelBox.appendChild(anchor);
-		tvBox.appendChild(channelBox);
-	
-		$$__Channels.set(modelName, channelBox);
-		scanChannel(modelName, img);
-	}
+    if (! $$__Channels.has(modelName)) {
+        let tvBox = document.getElementById("main");
+    
+        let channelBox;
+        if ($$__Params.getAll('shuffle').length > 0) {
+            let shuffledPriority = Math.floor(Math.random() * 100) + model.priority;
+            console.log(modelName + " -> " + shuffledPriority);
+            channelBox = createChannelBox(model, shuffledPriority);
+        } else {
+            channelBox = createChannelBox(model);
+        }
+    
+        let anchor = createAnchor(modelName);
+        let img = createImg(modelName);
+    
+        anchor.appendChild(img);
+        channelBox.appendChild(anchor);
+        tvBox.appendChild(channelBox);
+    
+        $$__Channels.set(modelName, channelBox);
+        scanChannel(modelName, img);
+    }
 }
 
 const $$__Tempus = document.getElementById("timestamp");
 function fugit() {
-	$$__Tempus.innerHTML = Date.now();
+    $$__Tempus.innerHTML = Date.now();
 }
 
 function tempusFugit() {
-	if ($$__Active.size == 0) {
-		if ($$__Tempus.style.display != 'inline') {
-			$$__Tempus.style.display = 'inline';
-		}
-		fugit();
-	} else {
-		if ($$__Tempus.style.display != 'none') {
-			$$__Tempus.style.display = 'none';
-		}
-	}
+    if ($$__Active.size == 0) {
+        if ($$__Tempus.style.display != 'inline') {
+            $$__Tempus.style.display = 'inline';
+        }
+        fugit();
+    } else {
+        if ($$__Tempus.style.display != 'none') {
+            $$__Tempus.style.display = 'none';
+        }
+    }
 }
 
 const $$__Throbber = document.getElementById("throbber");
 function toggleThrobber(display) {
-	$$__Throbber.style.display = display;
+    $$__Throbber.style.display = display;
 }
 function pulseThrobber() {
-	toggleThrobber('block');
-	setTimeout(toggleThrobber, 1024, 'none');
+    toggleThrobber('block');
+    setTimeout(toggleThrobber, 1024, 'none');
 }
 
 function renderChannels() {
-	$$__Models.forEach(renderChannel);
+    $$__Models.forEach(renderChannel);
 }
 
 setInterval(refreshing, 256);
