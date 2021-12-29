8e296c322e7a31fa8b9602a308afe46cd47e0a7a
Jonathan Strange <parallel42@gmail.com>
2021-12-29 03:58:18 -0500

    [cb0] ronandalice

--- cb0/models.js
+++ cb0/models.js
@@ -124,7 +124,8 @@ let tierC = [
     "poliina",
     "eva_nelson",
     "greeniccee",
-    "voighta"
+    "voighta",
+    "ronandalice"
 ];
 let fn__tierC = async () => { tierC.forEach(m => addModel(m, 30)); }
 fn__tierC();
