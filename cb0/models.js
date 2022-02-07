3b1a9c9041990486b3eb274d93f3b8517f02574c
Jonathan Strange <parallel42@gmail.com>
2022-02-07 06:46:02 -0500

    [cb0] cyber_geisha

--- cb0/models.js
+++ cb0/models.js
@@ -116,7 +116,8 @@ let tierC = [
     "dariadeli",
     "ur_kussy",
     "eva_j",
-    "lastdwan"
+    "lastdwan",
+    "cyber_geisha"
 ];
 let fn__tierC = async () => { tierC.forEach(m => addModel(m, 30)); }
 fn__tierC();
