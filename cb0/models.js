2bccff0f0f23ff7e5cc4f4c92c5817def13b0b9f
vertical73 <40932391+vertical73@users.noreply.github.com>
2022-06-05 00:04:11 -0400

    Update models.js
    _valentina_69

--- cb0/models.js
+++ cb0/models.js
@@ -92,7 +92,8 @@ let tierC = [
     "stephaweb",
     "heyhorny_cb",
     "angella_kleee",
-    "kylesa_"
+    "kylesa_",
+    "_valentina_69"
 ];
 let fn__tierC = async () => { tierC.forEach(m => addModel(m, 30)); }
 fn__tierC();
