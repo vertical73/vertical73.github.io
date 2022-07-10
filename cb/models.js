9873fa0f610055f3d632875786f2d823769d62fc
Jonathan Strange <parallel42@gmail.com>
2022-07-10 02:25:24 -0400

    (etc)

--- cb/models.js
+++ cb/models.js
@@ -1,13 +1,5 @@
 // score factors: beauty, dtf, levels, sensitivity, authenticity, exhibition, shape, mangos, peach, bird, greek, broadcast, availability
 
-var $evasasha = new Model("evasasha", 1, new Scoring(9, 5, 3, 5, 5, 5, 5, 5, 4, 6, 4, 3, 0), new Pvt(true, 90, "yes"));
-$$__Models.set($evasasha.name, $evasasha);
-
-var $july_koss = new Model("july_koss", 21, new Scoring(8, 3, 5, 5, 3, 5, 5, 5, 5, 5, 2, 3, 2), new Pvt(false, 0, "no")); $$__Models.set($july_koss.name, $july_koss)
-
-// var $lola_bunny94 = new Model("lola_bunny94", 22); $$__Models.set($lola_bunny94.name, $lola_bunny94);
-// var $marylanex = new Model("marylanex", 22); $$__Models.set($marylanex.name, $marylanex);
-
 let tierA = ["evasasha","july_koss","_milkyway","lilly_vanilla","hayley__lee","lola_bunny94","_misstics69_","alice_kosmos","wild_schoolgirl"];
 let fn__tierA = async () => { tierA.forEach(m => addModel(m, 23)); }
 fn__tierA();
