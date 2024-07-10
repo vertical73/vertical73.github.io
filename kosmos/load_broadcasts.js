93c8a76701be3e5313f3bf5bd9fdd6756ce50f46
Strange <strange@FOXTROT.local>
2024-07-10 05:54:49 -0400

    adjusts for javascript map stupidity

--- kosmos/load_broadcasts.js
+++ kosmos/load_broadcasts.js
@@ -4,7 +4,8 @@ function updateYmdMap(ymdMap, dateKey, broadcastDurationMilliseconds) {
     let broadcastDurationMinutes = (broadcastDurationMilliseconds / 1000 / 60);
     if (ymdMap.has(dateKey)) {
         let minutesForDate = ymdMap.get(dateKey);
-        ymdMap.set(dateKey, (minutesForDate + broadcastDurationMinutes));
+        let minutesForDateTotal = (minutesForDate + broadcastDurationMinutes);
+        ymdMap.set(dateKey, minutesForDateTotal);
     } else {
         ymdMap.set(dateKey, broadcastDurationMinutes);
     }
@@ -17,18 +18,18 @@ function processBroadcast(ymdMap, broadcast) {
     // check if start and end on same ymd
     if (startdt.getDate() === enddt.getDate()) {
         // if so, ymd -> (end-start)
-        let dateKey = new Date(startdt.getFullYear(), startdt.getMonth(), startdt.getDate());
+        let dateKey = (new Date(startdt.getFullYear(), startdt.getMonth(), startdt.getDate())).getTime();
         let broadcastDurationMilliseconds = (enddt.getTime() - startdt.getTime());
         updateYmdMap(ymdMap, dateKey, broadcastDurationMilliseconds);
     } else {
         // else calc from ymd date line (end date midnight) and make two entries
-        let startDateKey = new Date(startdt.getFullYear(), startdt.getMonth(), startdt.getDate());
-        let endDateKey = new Date(enddt.getFullYear(), enddt.getMonth(), enddt.getDate());
+        let startDateKey = (new Date(startdt.getFullYear(), startdt.getMonth(), startdt.getDate())).getTime();
+        let endDateKey = (new Date(enddt.getFullYear(), enddt.getMonth(), enddt.getDate())).getTime();
 
-        let startDateBroadcastDurationMilliseconds = (endDateKey.getTime() - startdt.getTime());
+        let startDateBroadcastDurationMilliseconds = (endDateKey - startdt.getTime());
         updateYmdMap(ymdMap, startDateKey, startDateBroadcastDurationMilliseconds);
     
-        let endDateBroadcastDurationMilliseconds = (enddt.getTime() - endDateKey.getTime());
+        let endDateBroadcastDurationMilliseconds = (enddt.getTime() - endDateKey);
         updateYmdMap(ymdMap, endDateKey, endDateBroadcastDurationMilliseconds);
     }
 }
