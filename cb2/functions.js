b1dc721a83aafeff314cdcfc4e08328ecdd28982
strange <vertical73@proton.me>
2024-11-16 20:30:29 -0500

    [cb2] debugging viewport width (03)

--- cb2/functions.js
+++ cb2/functions.js
@@ -1,7 +1,5 @@
 const $$__viewportWidth = window.innerWidth;
 
-window.title = window.title + " [" + $$__viewportWidth + "]";
-
 class Model {
     constructor(name, priority, scoring, pvt) {
         this.name = name;
