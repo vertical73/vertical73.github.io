ba525155c2d2934c3c20a1ab8d8411486400127b
0qr <69781600+0qr@users.noreply.github.com>
2025-11-16 07:02:32 -0500

    (sorting)

--- models/cb.js
+++ models/cb.js
@@ -182,7 +182,6 @@ let cb0Primary = [
     "lunainnk", // 🇺🇦
     "candyboofairy",
     "meowalice1", // 🇷🇴
-    "yononeey",
 
     // slutty appeal, 0-1
     "vilanelle_1",
@@ -247,9 +246,10 @@ let cb0Primary = [
     // strictly eye candy, 0-3
     "goodg_oddamn",
     "corni__",
-    "veronikaav"
+    "veronikaav",
 
     // getting to know, 0-4
+    "yononeey"
 ];
 
 let cb1Primary = [
